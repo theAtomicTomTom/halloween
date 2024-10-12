8a5faa4589b3d4f8fbb75097a7f0e6ec775a5a0d
theAtomicTomTom <tlujan94@rams.colostate.edu>
2024-10-12 11:41:34 -0600

    remove footer disclaimer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,6 @@ export default function Home() {
     { selectedTab === TabOptions.RSVP && <Table /> }
     { selectedTab === TabOptions.POTLUCK && <PotluckTable /> }
     { selectedTab === TabOptions.CHARACTER_SELECT && <CharacterSelectTable /> }
-    <div>Listen folks, still workin&rsquo; on this pile o&rsquo; crap. Please continue to use the spreadhseets here as applicable for potluck and email...</div>
-    <a href="https://docs.google.com/spreadsheets/d/1gv_6xxpc9XqAzg_5VCoG0D4f9dUh5d_nzw-oDJj2iDU/edit?gid=2128357706#gid=2128357706">google sheets (click me)</a>
-    <div>NOTE: if you cannot update from your phone, download the google sheets app or try from a computer.</div>
   </div>
   );
 }
